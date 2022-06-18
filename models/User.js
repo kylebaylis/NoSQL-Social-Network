413192387a383a8ed68e105af5ff35d955e4670b
Kyle <kylejmbaylis@gmail.com>
2022-06-18 15:17:45 -0400

    user model done

--- models/User.js
+++ models/User.js
@@ -1,23 +1,40 @@
 const {Schema, model} = require('mongoose');
 
+const emailValidation = (email) => {
+    const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return regex.test(email);
+};
+
 const UserSchema = new Schema(
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
         email: {
-
+            type: String,
+            unique: true,
+            required: 'Email address required',
+            validate: [emailValidation, 'Please enter a valid email'],
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email']
         },
-        thoughts: {
-
-        },
-        friends: {
-
+        thoughts: [ThoughtSchema],
+        friends: [UserSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
         }
     }
 );
 
 // friendCount virtual here
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 const User = model('User', UserSchema);
 
