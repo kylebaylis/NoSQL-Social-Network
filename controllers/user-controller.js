db862ad3acf66962e5de99947a74197ed2580ea8
Kyle <kylejmbaylis@gmail.com>
2022-06-20 16:07:12 -0400

    user routes all working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,17 +13,44 @@ const userController = {
 
     // get single user by id
     getUserById({params}, res) {
+        User.findOne({_id: params.id})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
 
+    // create new user
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     },
 
     // update single user by id
     updateUser({params, body}, res) {
-
+        User.findOneAndUpdate({_id: params.id}, body, {new: true})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user with that id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     },
 
     // delete single user by id
     deleteUser({params}, res) {
-
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 };
 
--- models/User.js
+++ models/User.js
@@ -33,7 +33,8 @@ const UserSchema = new Schema(
         toJSON: {
             virtuals: true,
             getters: true
-        }
+        },
+        id: false
     }
 );
 
